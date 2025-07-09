74df57889754a670614c29f9be517b098c77e07f
Vamshi <156120230+ganeshveluru819@users.noreply.github.com>
2025-07-09 14:26:59 -0400

    Added variables, Getter and Setter for Address Class

--- src/test/java/com/discover/DiscoverServiceApplicationTests.java
+++ src/test/java/com/discover/DiscoverServiceApplicationTests.java
@@ -1,4 +1,5 @@
 package com.discover;
+import com.discover.entity.Address;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,14 @@ class DiscoverServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	public static void main(String a[])
+	{
+		Address addr = new Address(1, "MG Road", "Hyderabad", "Telangana", "500081", "Active");
+		System.out.println("City: " + addr.getCity());
+        addr.setCity("Warangal");
+        System.out.println("Updated City: " + addr.getCity());
 	}
 
 }
