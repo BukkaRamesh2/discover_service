92e82ca01bd54bb47812fc0a6d509fc75423c3b2
harsha <70021323+hiking8464@users.noreply.github.com>
2025-07-09 13:31:09 -0400

    added Employees class

---
+++ src/main/java/com/discover/entity/Employees.java
@@ -0,0 +1,23 @@
+package com.discover.entity;
+
+public class Employees {
+	/*Employees -- harsha
+	      employeeId;
+			firstName;
+			lastName;
+			address;
+			email;
+			phoneNumber;
+			gender;
+			status;
+	*/
+	int employeeID;
+	String firstName;
+	String lastName;
+	String address;
+	String email;
+	int phoneNumber;
+	String gender;
+	String status;
+
+}
