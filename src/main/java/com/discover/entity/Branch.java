1eb5ac4e75496693ca778d32808c158d724b3db0
Jwala Srihari <61618519+jwalasrihari@users.noreply.github.com>
2025-07-09 14:08:08 -0400

    Added setter and getter

--- src/main/java/com/discover/entity/Branch.java
+++ src/main/java/com/discover/entity/Branch.java
@@ -1,10 +1,62 @@
 package com.discover.entity;
 
 public class Branch {
-	int branchId;
-	String branchName;
-	String branchManager;
-	String branchCode;
-	int timings;
-	String status;
+	private int branchId;
+	private String branchName;
+	private String branchManager;
+	private String branchCode;
+	private int timings;
+	private String status;
+	
+	public Branch(int branchId, String branchName, String branchManager, String branchCode, int timings,String status)
+	{
+		super();
+		this.branchId = branchId;
+		this.branchName = branchName;
+		this.branchManager = branchManager;
+		this.branchCode = branchCode;
+		this.timings = timings;
+		this.status = status;
+	}
+	
+	
+	
+	public int getBranchId() {
+		return branchId;
+	}
+	public void setBranchId(int branchId) {
+		this.branchId = branchId;
+	}
+	public String getBranchName() {
+		return branchName;
+	}
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+	public String getBranchManager() {
+		return branchManager;
+	}
+	public void setBranchManager(String branchManager) {
+		this.branchManager = branchManager;
+	}
+	public String getBranchCode() {
+		return branchCode;
+	}
+	public void setBranchCode(String branchCode) {
+		this.branchCode = branchCode;
+	}
+	public int getTimings() {
+		return timings;
+	}
+	public void setTimings(int timings) {
+		this.timings = timings;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
 }
