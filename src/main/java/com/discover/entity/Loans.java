8cdffe6c21a1bf12d693d9aca837cf9e4ff1bd9c
Sai Dinesh Reddy <212642022+SaiDinesh4745@users.noreply.github.com>
2025-07-09 18:55:20 -0500

    Added Loans class

---
+++ src/main/java/com/discover/entity/Loans.java
@@ -0,0 +1,86 @@
+package com.discover.entity;
+
+import java.util.Date;
+
+public class Loans {
+
+    //	loanId
+    //  loanType 
+    //  creditScore
+    //  loanAmount
+    //  apr
+    //  loanDate
+    //  status
+
+    private int loanId;
+    private String loanType;
+    private int creditScore;
+    private double loanAmount;
+    private double apr;
+    private String status;
+    private Date loanDate;
+
+    public Loans(int loanId, String loanType, int creditScore, double loanAmount, double apr, String status, Date loanDate) {
+        this.loanId = loanId;
+        this.loanType = loanType;
+        this.creditScore = creditScore;
+        this.loanAmount = loanAmount;
+        this.apr = apr;
+        this.status = status;
+        this.loanDate = loanDate;
+    }
+
+    public int getLoanId() {
+        return loanId;
+    }
+
+    public String getLoanType() {
+        return loanType;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public double getLoanAmount() {
+        return loanAmount;
+    }
+
+    public double getApr() {
+        return apr;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Date getLoanDate() {
+        return loanDate;
+    }
+
+
+    public void setLoanType(String loanType) {
+        this.loanType = loanType;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public void setLoanAmount(double loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public void setApr(double apr) {
+        this.apr = apr;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setLoanDate(Date loanDate) {
+        this.loanDate = loanDate;
+    }
+
+}
