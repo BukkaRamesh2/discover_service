724a59be882d3b9af537e01b66d19d66ff73d190
vijaya-pvl <vijayalakshmiputta234@gmail.com>
2025-07-09 20:25:20 -0500

    created reward attributes

---
+++ src/main/java/com/discover/entity/Rewards.java
@@ -0,0 +1,12 @@
+package com.discover.entity;
+
+import java.time.LocalDate;
+
+public class Rewards {
+
+	long rewardsId; 
+    String rewardsName;
+    int rewardsPoints;
+    LocalDate issueDate;
+	String status;
+}
