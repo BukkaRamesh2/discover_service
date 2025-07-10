e28a74f70fb57347e80857953e70763e2ae675aa
Hsv0345 <harikaprathipati14@gmail.com>
2025-07-09 23:19:00 -0500

    Account Attributes

---
+++ src/main/java/com/discover/entity/Account.java
@@ -0,0 +1,16 @@
+package com.discover.entity;
+
+import java.time.LocalDate;
+
+public class Account {
+
+	private Long accountId;
+    private String accountNumber;
+    private String accountType;
+    private Double balance;
+    private LocalDate createdDate;
+    private String status;
+    		
+}
+
+
