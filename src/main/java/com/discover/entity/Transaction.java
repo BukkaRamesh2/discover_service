6e1b464323763832392beee3a55c5d59e4808fc0
GuttikondaB <161375813+GuttikondaB@users.noreply.github.com>
2025-07-09 19:56:43 -0500

    Transaction Attributes

---
+++ src/main/java/com/discover/entity/Transaction.java
@@ -0,0 +1,14 @@
+package com.discover.entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+public class Transaction {
+	  
+	      int  transactionId;
+	      char  transactionType;
+	      LocalDate date;
+	      LocalTime time;
+	      float  amount;
+	      char branchCode;
+
+}
