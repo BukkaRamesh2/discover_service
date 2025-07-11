bd027f6649ca8079f7c2ceaf1dd84c90b0489222
MSaikrishna73 <manukondasaikrishna73@gmail.com>
2025-07-10 23:08:54 -0500

    1st class assignment: given all attribute for fee.java

---
+++ src/main/java/com/discover/entity/Fee.java
@@ -0,0 +1,88 @@
+package com.discover.entity;
+
+import java.time.LocalDate;
+
+public class Fee {
+	
+	/*
+	 * feeId - unique ID for all fees generate in bank similar to transactionID
+	 * feeName - short name where customer can see what kind of fee they got eg: Over Draft, late payment etc.
+	 * amount - what is the fee amount.
+	 * deadline - The date by which the fee must be paid.
+	 * Active - Is fee is still active true: customer should pay. false : paid or not yet assigned.
+	 * description - explain why fee is added to their account or describing the fee policies.
+	 */
+
+    private String feeId;
+
+    private String feeName;
+
+    private long amount;
+
+    private LocalDate deadline;
+
+    private boolean Active;
+
+    private String description;
+
+    public Fee() {
+        
+    }
+
+    public Fee(String feeId, String feeName, long amount, LocalDate deadline, boolean Active, String description) {
+        this.feeId = feeId;
+        this.feeName = feeName;
+        this.amount = amount;
+        this.deadline = deadline;
+        this.Active = Active;
+        this.description = description;
+    }
+
+    public String getFeeId() {
+        return feeId;
+    }
+
+    public String getFeeName() {
+        return feeName;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public boolean getActive() {
+        return Active;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setFeeId(String feeId) {
+        this.feeId = feeId;
+    }
+
+    public void setFeeName(String feeName) {
+        this.feeName = feeName;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public void setActive(boolean active) {
+        this.Active = active;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
