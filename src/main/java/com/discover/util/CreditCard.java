26d3b25404ca76406db91e7626d8af9e3cc787b8
kakileti24 <161528985+kakileti24@users.noreply.github.com>
2025-07-12 23:37:12 -0400

    Implemented java basics and oops concepts

---
+++ src/main/java/com/discover/util/CreditCard.java
@@ -0,0 +1,66 @@
+package com.discover.util;
+
+import java.util.Date;
+
+import com.discover.entity.Cards;
+
+public class CreditCard extends Cards implements CardOperations {
+	
+	public CreditCard(long cardId, String cardNumber, double CardLimit, Date issueDate, Date expiryDate) {
+		super(cardId, cardNumber, "Credit",CardLimit, "Inactive",issueDate, expiryDate);
+	}
+	
+	//Method Overriding...........
+	
+	@Override
+	public void activateCard()
+	{
+		setStatus("Active");
+		System.out.println("Credit Card Activated");
+	}
+	
+	@Override
+	public void deactivateCard()
+	{
+		setStatus("Inactive");
+		System.out.println("Credit Card Deactivated");
+	}
+	@Override
+	public double checkLimit()
+	{
+		return getCardLimit();
+	}
+	
+	
+	//Method Overloading.......
+	public void displayCardInfo(Cards card) {
+		System.out.println("Card Number: " + card.getCardNumber());
+        System.out.println("Card Type: " + card.getCardType());
+        System.out.println("Card Status: " + card.getStatus()); 
+	    }
+
+	public void displayCardInfo(Cards card, boolean showLimit) {
+		displayCardInfo(card);
+	    if (showLimit) {
+	    	System.out.println("Card Limit: " + card.getCardLimit());
+	     }
+	    }
+
+   public void checkStatus(Cards card) {
+	   //Branching statements
+	   switch (card.getStatus()) {
+	   case "Active":
+		   System.out.println("Card is Active");
+		   break;
+        case "Inactive":
+            System.out.println("Card is Inactive");
+            break;
+        default:
+            System.out.println("Unknown Card Status");
+	        }
+	    }	
+	
+	
+
+
+}
