e909de0663d8d10711a0dc9a0c3e9b96b5df19cd
vkReddy2000 <jakkavamshikrishnareddy@gmail.com>
2025-07-14 13:08:34 -0500

    added getter setter method

--- src/main/java/com/discover/entity/Category.java
+++ src/main/java/com/discover/entity/Category.java
@@ -2,13 +2,50 @@ package com.discover.entity;
 
 public class Category {
 
-	private int ID;
-    boolean type;
-	long date;
-	boolean status;	
-	
+   private int categoryID;
+   protected boolean categorytype;
+   private long  categorydate;
+   private boolean categorystatus;
 
-	
-		
 
+
+   
+   
+
+
+    public int getcategoryID() {
+        return categoryID;
+    }
+
+    public void setcategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    
+    public boolean categorytype() {
+        return categorytype;
+    }
+
+    public void setcategorytype(boolean categorytype) {
+        this.categorytype = categorytype;
+    }
+
+    
+    public long getcategorydate() {
+		return categorydate;
+	}
+
+	public void categorydate(long categorydate) {
+		this.categorydate = categorydate;
+	}
+    
+    public boolean  categoryStatus() {
+        return categorystatus;
+    }
+
+    public void setcategoryStatus(boolean categorystatus) {
+        this.categorystatus = categorystatus;
+    
+    }
+   
 }
