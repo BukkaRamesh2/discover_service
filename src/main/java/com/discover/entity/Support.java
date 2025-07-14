3cb34255f87bc81a6ae83d92253a67541715b7a2
srinath_siripuram <srinath.siripuram@tetruscorp.com>
2025-07-14 23:56:41 +0530

    added getter and setter methods, implemented abstraction & inheritance

---
+++ src/main/java/com/discover/entity/Support.java
@@ -0,0 +1,62 @@
+package com.discover.entity;
+
+import java.util.Date;
+
+public class Support {
+	private int supportId;
+    private String customerName;
+    private String description;
+    private Date createDate;
+    private String status;
+
+    // Constructor
+    public Support(int supportId, String customerName, String description, Date date, String status) {
+        this.supportId = supportId;
+        this.customerName = customerName;
+        this.description = description;
+        this.createDate = date;
+        this.status = status;
+    }
+    
+    public int getSupportId() {
+        return supportId;
+    }
+
+    public void setSupportId(int supportId) {
+        this.supportId = supportId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+ 
+}
---
+++ src/test/java/com/discover/TestSupport.java
@@ -0,0 +1,20 @@
+package com.discover;
+import com.discover.SupportOperation.SupportProcess;
+import com.discover.entity.Support;
+
+
+import java.util.Date;
+
+public class TestSupport {
+    public static void main(String[] args) {
+        Support support = new Support(101, "John Doe", "Issue with login", new Date(), "Open");
+
+        SupportProcess process = new SupportProcess(support);
+
+        process.checkStatus();
+        process.displayInfo();
+        process.closeSupport();
+        process.getByName();
+        }
+
+}
