2661d20afffb3f061ff96ed1fda6964b28a162a4
Jwala Srihari <61618519+jwalasrihari@users.noreply.github.com>
2025-07-15 13:58:49 -0400

    Implemented Controllers,Services,Repo

---
+++ src/main/java/com/discover/service/BranchService.java
@@ -0,0 +1,13 @@
+package com.discover.service;
+
+import java.util.List;
+
+import com.discover.entity.Branch;
+
+public interface BranchService {
+
+	public Branch addBranch(Branch brh);
+	public Branch getBranch(Long Id);
+	public List<Branch> getAllBranches();
+	public Branch updateCustomer(Branch brh);
+}
