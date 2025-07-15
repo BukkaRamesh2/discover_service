b9239ad4dd50636b3a78aed21bf1d1a374989ffc
lokesh kakarla <219924874+leelakakarla@users.noreply.github.com>
2025-07-15 15:34:33 -0400

    ADD Test.java

--- src/main/java/com/discover/util/Test.java
+++ src/main/java/com/discover/util/Test.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //package com.discover.util;
 //
 //import com.discover.entity.Customer;
@@ -81,3 +82,50 @@
 //	}
 //
 //}
+=======
+package com.discover.util;
+
+import com.discover.entity.Customer;
+import com.discover.entity.DiscoverCustomer;
+
+public class Test extends Customer{
+	
+	
+	public String testData;  //
+	
+	
+	public DiscoverCustomer customer;    // aggrigation 
+	
+	
+	public void testMethod() {
+		System.out.println("inside test class");
+	}
+	
+	
+	public Test(Long customerID, String firstName, String lastName, String address, String email, Long phoneNumber,
+			boolean gender, boolean status, String name, Integer age, DiscoverCustomer customer) {
+		super(customerID, firstName, lastName, address, email, phoneNumber, gender, status, name, age);
+		this.customer = customer;
+	}
+
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public static void main(String[] args) {
+		Customer c = new Customer();   // 4% , 5%, 7%
+		Test  t = new Test();
+		//t.customer.getDiscCustomer(); //  
+	}
+	
+	
+	public void test(){
+	}
+<<<<<<< HEAD
+=======
+
+>>>>>>> 6b0a652 (adding Test.java)
+}
+>>>>>>> 1faf4c9 (Add Test.java)
