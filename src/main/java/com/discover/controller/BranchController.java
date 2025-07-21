d89b089e8f57bf7ece1962fb11880d500c7fd5c9
Jwala Srihari <61618519+jwalasrihari@users.noreply.github.com>
2025-07-21 18:29:22 -0400

    DB and Swagger UI testing

--- src/main/java/com/discover/controller/BranchController.java
+++ src/main/java/com/discover/controller/BranchController.java
@@ -3,6 +3,8 @@ package com.discover.controller;
 import com.discover.entity.Branch;
 import com.discover.service.BranchService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,43 +13,58 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/branch")
+@Tag(name = "Branch API", description = "Operations related to Branches")
 public class BranchController {
 
 	@Autowired
 	BranchService brhService;
 	
-	@PostMapping("/addBranch")
-	public Branch addBranch(@RequestBody Branch brh)
-	{
-		return brhService.addBranch(brh);
-	}
-	
-	@GetMapping("/getBranch/{branchID}")
-	public Branch getBranch(@PathParam("branchID") Long branchID)
-	{
-		return brhService.getBranch(branchID);
-	}
-	
 	@GetMapping("/getAllBranches")
-	public List<Branch> getAllBranch()
+	@Operation(summary = "Get all Branches")
+	public List<Branch> getAllBranches()
 	{
 		return brhService.getAllBranches();
 	}
 	
-	@PutMapping("/updateBranch")
-	public Branch updateBranch(@RequestBody Branch brh)
+	@GetMapping("/getBranch/{branchID}")
+	@Operation(summary = "Get Branch by ID")
+	public ResponseEntity<Branch> getBranch(@PathParam("branchID") Long branchID)
 	{
-		return brhService.updateBranch(brh);
+		Branch branch = brhService.getBranch(branchID);
+		return ResponseEntity.ok(branch);
+	}
+	
+	
+	
+	@PostMapping("/addBranch")
+	@Operation(summary = "Add Branch")
+	public ResponseEntity<Branch> addBranch(@RequestBody Branch brh)
+	{
+		Branch branch_to_save = brhService.addBranch(brh);
+		return new ResponseEntity<>(branch_to_save, HttpStatus.CREATED);
+	}
+	
+	
+	@PutMapping("/updateBranch")
+	@Operation(summary = "Update Branch")
+	public ResponseEntity<Branch> updateBranch(@RequestBody Branch brh)
+	{
+		Branch to_update = brhService.updateBranch(brh);
+		return ResponseEntity.ok(to_update);
 	}
 	
 	@DeleteMapping("/deleteBranch/{branchID}")
-	public void deleteBranch()
+	@Operation(summary = "Delete Branch by ID")
+	public ResponseEntity<Void> deleteBranch(@PathParam("branchID") Long branchID)
 	{
-		
+		brhService.deleteBranch(branchID);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/discover/service/BranchServiceImpl.java
+++ src/main/java/com/discover/service/BranchServiceImpl.java
@@ -2,11 +2,17 @@ package com.discover.service;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 
 import com.discover.entity.Branch;
+import com.discover.exception.BranchNotFoundException;
+import com.discover.exception.CustomerNotFoundException;
 import com.discover.repository.BranchRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class BranchServiceImpl implements BranchService{
@@ -14,18 +20,43 @@ public class BranchServiceImpl implements BranchService{
 	@Autowired
 	BranchRepository brhRepo;
 	
+	@Transactional
 	@Override
 	public Branch addBranch(Branch brh)
 	{
+		if (brh.getBranchId() != null || brh.getVersion() != null) {
+            throw new IllegalArgumentException("No ID/version allowed when creating");
+        }
 		return brhRepo.save(brh);
+	}
+	
+	
+	@Transactional
+	@Override
+	public Branch updateBranch(Branch brh)
+	{
+		if (brh.getBranchId() == null || brh.getVersion() == null) {
+            throw new IllegalArgumentException("ID and version required for update");
+        }
+
+        Branch existing = brhRepo.findById(brh.getBranchId())
+            .orElseThrow(() -> new CustomerNotFoundException("Branch Id " + brh.getBranchId() + " not found"));
+
+        if (!existing.getVersion().equals(brh.getVersion())) {
+            throw new ObjectOptimisticLockingFailureException(Branch.class, brh.getBranchId());
+        }
+
+        BeanUtils.copyProperties(brh, existing, "id", "version");
+		return brhRepo.save(existing);
 	}
 	
 	@Override
 	public Branch getBranch(Long Id)
 	{
-		return brhRepo.getById(Id);
+		return brhRepo.findById(Id)
+				.orElseThrow(() -> new BranchNotFoundException("Branch Id " + Id + " not found"));
 	}
-	
+
 	@Override
 	public List<Branch> getAllBranches()
 	{
@@ -33,9 +64,12 @@ public class BranchServiceImpl implements BranchService{
 	}
 	
 	@Override
-	public Branch updateBranch(Branch brh)
-	{
-		return brhRepo.save(brh);
+	public void deleteBranch(Long Id) {
+		if (!brhRepo.existsById(Id)) {
+            throw new BranchNotFoundException("Branch Id " + Id + " not found");
+        }
+        brhRepo.deleteById(Id);
+		
 	}
 
 }
